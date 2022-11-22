4374f444fcf81f103af275d7c1238e21ed4292f5
Celine Brin <celine-brin@hotmail.fr>
2022-11-22 17:40:43 +0100

    page du dragon, controller dragon avec skip, proposition de couleurs dans assets, EN COURS

--- app/controllers/dragons_controller.rb
+++ app/controllers/dragons_controller.rb
@@ -1,7 +1,12 @@
 class DragonsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index show]
 
   def index
     @dragons = Dragon.all
+  end
+
+  def show
+    @dragon = Dragon.find(params[:id])
   end
 
 private
