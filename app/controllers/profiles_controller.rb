ad3e2f56965706eb27e8197279062db6bd43c8b9
Denis Velic <denis.velic@laposte.net>
2022-11-25 15:55:40 +0100

    oui

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,13 +2,12 @@ class ProfilesController < ApplicationController
   before_action :set_user, only: [:show]
 
   def show
-    @dragons = @user.dragons
+    @dragons = Dragon.where(owner: current_user)
+    # @dragons = @user.dragons
     @bookings = @user.bookings
 
     @reservations = Booking.includes(:dragon).where(dragons: { owner: @user })
   end
-
-  
 
   private
 
